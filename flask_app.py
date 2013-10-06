0170db02b8411fda27cdf0eac99b86f4a8130a76
synbiozis <synbiozisnetwork@gmail.com>
2013-10-06 15:41:20 +0200

    New update

--- flask_app.py
+++ flask_app.py
@@ -5,14 +5,28 @@ from flask import Flask
 from os import walk
 import flask
 app = Flask(__name__)
+
+app.secret_key = "test"
  
 @app.route('/')
 def index():
-    return flask.render_template('bones.html')
- 
-@app.route('/news/')
-def news():
+
 	return flask.render_template('news.html')
+	
+@app.route('/videos/')
+def clip():
+	return flask.render_template('clip.html')
+
+@app.route('/test/')
+def test():
+	return flask.render_template('test.html')
+	
+@app.route('/bones/')
+def bones():
+	flask.flash(u'Vous etes arrivés aux news !', 'success')
+	flask.flash(u'Merde, il y a un probleme...', 'error')
+	return flask.render_template('bones.html')
+
 
 @app.context_processor
 def toTemplates():
@@ -28,8 +42,22 @@ def toTemplates():
 	def readfile(f):
 		with open('static/news/'+str(f), 'r') as f:
 			return f.read().split("\n")
+
+	def videos():
+		with open('static/videos/videos', 'r') as f:
+			return f.read().split("\n")
 			
-	return dict(news=newsfile(), readfile=readfile)
+	return dict(news=newsfile(), readfile=readfile, videos=videos())
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
